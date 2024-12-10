494b9605eee82adf8714e24afa32152b17a60387
Tugce Dalay <tgcdalay@gmail.com>
2024-12-10 23:12:57 +0300

    jobtitles

--- hrms/src/main/java/com/tobeto/entities/concretes/JobTitles.java
+++ hrms/src/main/java/com/tobeto/entities/concretes/JobTitles.java
@@ -1,7 +1,5 @@
 package com.tobeto.entities.concretes;
 
-import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
